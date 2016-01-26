4db58b4a11f909cb948f8e5bb21533459e8d58fe
Mahdi Mohammadi <mm@binary.com>
2016-01-26 01:37:36 +0000

    adding unit test and original module

--- lib/Data/Chronicle.pm
+++ lib/Data/Chronicle.pm
@@ -6,7 +6,7 @@ use warnings;
 
 =head1 NAME
 
-Data::Chronicle - The great new Data::Chronicle!
+Data::Chronicle - Provides efficient data storage for volatile and time-based data
 
 =head1 VERSION
 
@@ -16,37 +16,235 @@ Version 0.01
 
 our $VERSION = '0.01';
 
+=head1 DESCRIPTION
 
-=head1 SYNOPSIS
+This module contains helper methods which can be used to store and retrieve information
+on an efficient storage with below properties:
 
-Quick summary of what the module does.
+=over 4
 
-Perhaps a little code snippet.
+=item B<Timeliness>
 
-    use Data::Chronicle;
+It is assumed that data to be stored are time-based meaning they change over time and the latest version is most important for us.
+Many data structures in our system fall into this category (For example Volatility Surfaces, Interest Rate information, ...).
 
-    my $foo = Data::Chronicle->new();
-    ...
+=item B<Efficient>
 
-=head1 EXPORT
+The module uses Redis cache to provide efficient data storage and retrieval.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=item B<Persistent>
 
-=head1 SUBROUTINES/METHODS
+In addition to caching every incoming data, it is also stored in PostgresSQL for future retrieval.
 
-=head2 function1
+=item B<Distributed>
+
+These data are stored in distributed storage so they will be replicated to other servers instantly.
+
+=item B<Transparent>
+
+This modules hides all the details about distribution, caching, database structure and ... from developer. He only needs to call a method
+to save data and another method to retrieve it. All the underlying complexities are handled by the module.
+
+=back
+
+There are three important methods this module provides:
+
+=over 4
+
+=item C<set>
+
+Given a category, name and value stores the JSONified value in Redis and PostgreSQL database under "category::name" group and also stores current
+system time as the timestamp for the data (Which can be used for future retrieval if we want to get data as of a specific time). Note that the value
+MUST be either hash-ref or array-ref.
+
+=item C<get>
+
+Given a category and name returns the latest version of the data according to current Redis cache
+
+=item C<get_for>
+
+Given a category, name and timestamp returns version of data under "category::name" as of the given date (using a DB lookup).
+
+=back
+
+=head1 Example
+
+ my $d = get_some_data();
+
+ #store data into Chronicle
+ BOM::System::Chronicle::set("vol_surface", "frxUSDJPY", $d);
+
+ #retrieve latest data stored for "vol_surface" and "frxUSDJPY"
+ my $dt = BOM::System::Chronicle::set("vol_surface", "frxUSDJPY");
+
+ #find vol_surface for frxUSDJPY as of a specific date
+ my $some_old_data = get_for("vol_surface", "frxUSDJPY", $epoch1);
 
 =cut
 
-sub function1 {
+#we cache connection to Postgres, so we use state feature.
+use feature "state";
+
+#used for loading chronicle config file which contains connection information
+use YAML::XS;
+use JSON;
+use DBI;
+use DateTime;
+use Date::Utility;
+
+
+=head3 C<< set("category1", "name1", $value1)  >>
+
+Store a piece of data "value1" under key "category1::name1" in Pg and Redis.
+
+=cut
+
+sub set {
+    my $category = shift;
+    my $name     = shift;
+    my $value    = shift;
+    my $rec_date = shift;
+
+    $rec_date //= Date::Utility->new();
+
+    die "Cannot store undefined values in Chronicle!" unless defined $value;
+    die "You can only store hash-ref or array-ref in Chronicle!" unless (ref $value eq 'ARRAY' or ref $value eq 'HASH');
+
+    $value = JSON::to_json($value);
+
+    my $key = $category . '::' . $name;
+    BOM::System::RedisReplicated::redis_write()->set($key, $value);
+    _archive($category, $name, $value, $rec_date) if _dbh();
+
+    return 1;
 }
 
-=head2 function2
+=head3 C<< my $data = get("category1", "name1") >>
+
+Query for the latest data under "category1::name1" from Redis.
 
 =cut
 
-sub function2 {
+sub get {
+    my $category = shift;
+    my $name     = shift;
+
+    my $key         = $category . '::' . $name;
+    my $cached_data = BOM::System::RedisReplicated::redis_read()->get($key);
+
+    return JSON::from_json($cached_data) if defined $cached_data;
+    return;
+}
+
+=head3 C<< my $data = get_for("category1", "name1", 1447401505) >>
+
+Query Pg archive for the data under "category1::name1" at or exactly before the given epoch/Date::Utility.
+
+=cut
+
+sub get_for {
+    my $category = shift;
+    my $name     = shift;
+    my $date_for = shift;    #epoch or Date::Utility
+
+    my $db_timestamp = Date::Utility->new($date_for)->db_timestamp;
+
+    my $db_data = _dbh()->selectall_hashref(q{SELECT * FROM chronicle where category=? and name=? and timestamp<=? order by timestamp desc limit 1},
+        'id', {}, $category, $name, $db_timestamp);
+
+    return if not %$db_data;
+
+    my $id_value = (sort keys %{$db_data})[0];
+    my $db_value = $db_data->{$id_value}->{value};
+
+    return JSON::from_json($db_value);
+}
+
+sub get_for_period {
+    my $category = shift;
+    my $name     = shift;
+    my $start    = shift;    #epoch or Date::Utility
+    my $end      = shift;    #epoch or Date::Utility
+
+    my $start_timestamp = Date::Utility->new($start)->db_timestamp;
+    my $end_timestamp   = Date::Utility->new($end)->db_timestamp;
+
+    my $db_data =
+        _dbh()->selectall_hashref(q{SELECT * FROM chronicle where category=? and name=? and timestamp<=? AND timestamp >=? order by timestamp desc},
+        'id', {}, $category, $name, $end_timestamp, $start_timestamp);
+
+    return if not %$db_data;
+
+    my @result;
+
+    for my $id_value (keys %$db_data) {
+        my $db_value = $db_data->{$id_value}->{value};
+
+        push @result, JSON::from_json($db_value);
+    }
+
+    return \@result;
+}
+
+sub _archive {
+    my $category = shift;
+    my $name     = shift;
+    my $value    = shift;
+    my $rec_date = shift;
+
+    # In unit tests, we will use Test::MockTime to force Chronicle to store hostorical data
+    my $db_timestamp = $rec_date->db_timestamp;
+
+    return _dbh()->prepare(<<'SQL')->execute($category, $name, $value, $db_timestamp);
+WITH ups AS (
+    UPDATE chronicle
+       SET value=$3
+     WHERE timestamp=$4
+       AND category=$1
+       AND name=$2
+ RETURNING *
+)
+INSERT INTO chronicle (timestamp, category, name, value)
+SELECT $4, $1, $2, $3
+ WHERE NOT EXISTS (SELECT * FROM ups)
+SQL
+}
+
+#According to discussions made, we are supposed to support "Redis only" installation where there is not Pg.
+#The assumption is that we have Redis for all data which is important for continutation of our services
+#We also have Pg for an archive of data used later for non-live services (e.g back-testing, auditing, ...)
+#And in case for any reason, Redis has problems, we will need to re-populate its information not from Pg
+#But by re-running population scripts
+sub _dbh {
+    #silently ignore if there is not configuration for Pg chronicle (e.g. in Travis)
+    return if not defined _config()->{chronicle};
+
+    state $dbh = DBI->connect_cached(
+        "dbi:Pg:dbname=chronicle;port=6432;host=/var/run/postgresql",
+        "write", '',
+        {
+            RaiseError => 1,
+        });
+    return $dbh;
+}
+
+sub flush {
+}
+
+sub _config {
+    state $config = YAML::XS::LoadFile('/etc/rmg/chronicle.yml');
+    return $config;
+}
+
+# this code should be deleted after some time
+sub _redis_read {
+    warn "Chronicle::_redis_read is deprecated. Please, use RedisReplicated::redis_read";
+    return BOM::System::RedisReplicated::redis_read;
+}
+
+sub _redis_write {
+    warn "Chronicle::_redis_write is deprecated. Please, use RedisReplicated::redis_write";
+    return BOM::System::RedisReplicated::redis_write;
 }
 
 =head1 AUTHOR
