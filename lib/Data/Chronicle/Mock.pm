5184c72adf57fe4fefda32916e7551b1ef655d25
CraigLawrence <craig@binary.com>
2018-07-25 03:15:49 +0000

    fix for RedisDB compatibility

--- lib/Data/Chronicle/Mock.pm
+++ lib/Data/Chronicle/Mock.pm
@@ -16,6 +16,10 @@ use Test::Mock::Redis;
 use Data::Chronicle::Reader;
 use Data::Chronicle::Writer;
 
+use Test::MockModule;
+my $mocked_mock_redis = Test::MockModule->new('Test::Mock::Redis');
+$mocked_mock_redis->mock('mget', sub { return [ $mocked_mock_redis->original('mget')->(@_) ] });
+
 ## VERSION
 
 =head3 C<< my $ch = get_mocked_chronicle(); >>
